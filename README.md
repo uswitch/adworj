43c2ef3dd7a8202f3cc8fee38591ac637d81e9f2
Paul Ingles <paul@oobaloo.co.uk>
2015-01-22 14:55:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# adworj
+
+Clojure library to ease interacting with the Google AdWords API.
+
+## License
+
+Copyright &copy; 2015 uSwitch Limited.
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject adworj "0.1.0-SNAPSHOT"
+  :description "Clojure library to ease interacting with the Google AdWords API"
+  :url "https://github.com/uswitch/adworj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
