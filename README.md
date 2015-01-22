c0eab2af3063bd8868a14274ea47d828bd1edf5a
Paul Ingles <paul@oobaloo.co.uk>
2015-01-22 14:58:08 +0000

    add google adwords lib dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # adworj
 
-Clojure library to ease interacting with the Google AdWords API.
+Clojure library to ease interacting with the Google AdWords API. It is built upon [Google's AdWords API Java Client Library](https://github.com/googleads/googleads-java-lib).
 
 ## License
 
--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/uswitch/adworj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.google.api-ads/ads-lib "1.36.0"]
+                 [com.google.api-ads/adwords-axis "1.36.0"]])
