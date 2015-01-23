a096bb996d9f5847d4fc1a1d72f007a24a21ca5f
Paul Ingles <paul@oobaloo.co.uk>
2015-01-23 14:39:41 +0000

    add test for search-query report type

--- test/adworj/reporting_test.clj
+++ test/adworj/reporting_test.clj
@@ -2,7 +2,8 @@
   (:require [adworj.reporting :as r]
             [clj-time.format :as tf]
             [clojure.test :refer :all])
-  (:import [com.google.api.ads.adwords.lib.jaxb.v201409 ReportDefinitionDateRangeType]))
+  (:import [com.google.api.ads.adwords.lib.jaxb.v201409 ReportDefinitionDateRangeType]
+           [com.google.api.ads.adwords.lib.jaxb.v201409 ReportDefinitionReportType]))
 
 (deftest report-definition-test
   (let [d (r/report-definition r/search-query "pauls test")]
@@ -13,4 +14,6 @@
                                :range (r/date-range start-day end-day))]
     (is (= ReportDefinitionDateRangeType/CUSTOM_DATE (.getDateRangeType d)))
     (is (= "20150101" (.. d getSelector getDateRange getMin)))
-    (is (= "20150102" (.. d getSelector getDateRange getMax)))))
+    (is (= "20150102" (.. d getSelector getDateRange getMax))))
+  (is (= ReportDefinitionReportType/SEARCH_QUERY_PERFORMANCE_REPORT
+         (.getReportType (r/report-definition r/search-query "testing")))))
