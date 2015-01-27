1ebef1b114f530fbba32ee83269965eb820cb70c
Paul Ingles <paul@oobaloo.co.uk>
2015-01-27 12:11:18 +0000

    add age range report; fix tests

--- test/adworj/core_test.clj
+++ test/adworj/core_test.clj
@@ -1,7 +1,3 @@
 (ns adworj.core-test
   (:require [clojure.test :refer :all]
             [adworj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/adworj/reporting_test.clj
+++ test/adworj/reporting_test.clj
@@ -6,24 +6,23 @@
            [com.google.api.ads.adwords.lib.jaxb.v201409 ReportDefinitionReportType]))
 
 (deftest report-definition-test
-  (let [d (r/report-definition r/search-query "pauls test")]
+  (let [d (r/report-definition r/search-query-performance "pauls test")]
     (is (= ReportDefinitionDateRangeType/YESTERDAY (.getDateRangeType d))))
   (let [start-day (tf/parse "2015-01-01")
         end-day   (tf/parse "2015-01-02")
-        d (r/report-definition r/search-query "custom range test"
+        d (r/report-definition r/search-query-performance "custom range test"
                                :range (r/date-range start-day end-day))]
     (is (= ReportDefinitionDateRangeType/CUSTOM_DATE (.getDateRangeType d)))
     (is (= "20150101" (.. d getSelector getDateRange getMin)))
     (is (= "20150102" (.. d getSelector getDateRange getMax)))
-    (is (= 15 (count (.. d getSelector getFields)))))
+    (is (= 49 (count (.. d getSelector getFields)))))
   (is (= ReportDefinitionReportType/SEARCH_QUERY_PERFORMANCE_REPORT
-         (.getReportType (r/report-definition r/search-query "testing"))))
-  (is (false? (.isIncludeZeroImpressions (r/report-definition r/search-query "testing")))))
+         (.getReportType (r/report-definition r/search-query-performance "testing"))))
+  (is (false? (.isIncludeZeroImpressions (r/report-definition r/search-query-performance "testing")))))
 
 (deftest report-field-mapping-test
-  (is (= "AdGroupName"  (get-in r/search-query [:field-mappings :ad-group-name]) ))
-  (is (= :ad-group-name (get-in r/search-query [:reverse-mappings "AdGroupName"]))))
+  (is (= "AdGroupName"  (get-in r/search-query-performance [:field-mappings :ad-group-name]) )))
 
 (deftest report-field-names-test
-  (is (= 15 (count (r/selected-field-names r/search-query))))
-  (is (= "AdGroupName" (first (r/selected-field-names r/search-query)))))
+  (is (= 49 (count (r/selected-field-names r/search-query-performance))))
+  (is (= "AccountCurrencyCode" (first (r/selected-field-names r/search-query-performance)))))
