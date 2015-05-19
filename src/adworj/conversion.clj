db3a8410124597085a97e0843bfbf6e41b72afbb
Paul Ingles <paul@oobaloo.co.uk>
2015-05-19 12:07:11 +0100

    add upload-conversions to import offline conversion events into adwords

--- src/adworj/conversion.clj
+++ src/adworj/conversion.clj
@@ -1,5 +1,7 @@
 (ns adworj.conversion
-  (:import [com.google.api.ads.adwords.axis.v201409.cm ConversionTrackerCategory UploadConversion ConversionTrackerOperation ConversionTrackerServiceInterface Operator]
+  (:require [clj-time.core :as tc]
+            [clj-time.format :as tf])
+  (:import [com.google.api.ads.adwords.axis.v201409.cm OfflineConversionFeed OfflineConversionFeedOperation OfflineConversionFeedServiceInterface ConversionTrackerCategory UploadConversion ConversionTrackerOperation ConversionTrackerServiceInterface Operator]
            [com.google.api.ads.adwords.axis.factory AdWordsServices]))
 
 
@@ -42,3 +44,38 @@
   (let [service (tracker-service session)
         ops (map add-conversion-op upload-conversions)]
     (.mutate service (into-array ConversionTrackerOperation ops))))
+
+
+;; once you've defined a conversion type you can report conversions through
+;; the offline conversion feed service
+
+(def conversion-time-format (tf/formatter "yyyyMMdd HHmmss Z"))
+
+(defn conversion-feed
+  "represents an individual conversion event; associated to an ad click through
+   the gclid (google click id) parameter"
+  [name gclid & {:keys [time value currency-code]
+                 :or   {time          (tc/now)
+                        value         0.0
+                        currency-code "GBP"}}]
+  (doto (OfflineConversionFeed. )
+    (.setConversionName name)
+    (.setConversionTime (tf/unparse conversion-time-format time))
+    (.setConversionValue value)
+    (.setConversionCurrencyCode currency-code)
+    (.setGoogleClickId gclid)))
+
+(defn- conversion-feed-service [session]
+  (let [adwords (AdWordsServices. )]
+    (.get adwords session OfflineConversionFeedServiceInterface)))
+
+(defn- add-feed-op [conversion-feed]
+  (doto (OfflineConversionFeedOperation. )
+    (.setOperator Operator/ADD)
+    (.setOperand conversion-feed)))
+
+(defn upload-conversions
+  [session conversion-feeds]
+  (let [service (conversion-feed-service session)
+        ops (map add-feed-op conversion-feeds)]
+    (.mutate service (into-array OfflineConversionFeedOperation ops))))
