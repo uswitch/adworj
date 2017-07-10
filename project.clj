0b298cfc48f4835cf92dbe9bb461f4f1919fc180
Paul Ingles <paul@oobaloo.co.uk>
2017-07-10 10:38:24 +0100

    bump release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adworj "0.3.5"
+(defproject adworj "0.4.0"
   :description "Clojure library to ease interacting with the Google AdWords API"
   :url "https://github.com/uswitch/adworj"
   :license {:name "Eclipse Public License"
