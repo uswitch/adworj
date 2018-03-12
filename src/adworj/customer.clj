aa26ce01bf4a9867e59d641c52be23fecc7d4c39
Christian Blunden <christian.blunden@gmail.com>
2018-03-12 13:47:06 +0000

    upgrade to v201802 adwords api. checked migration docs and made necessary changes

--- src/adworj/customer.clj
+++ src/adworj/customer.clj
@@ -1,18 +1,19 @@
 (ns adworj.customer
-  (:import [com.google.api.ads.adwords.axis.v201705.mcm CustomerServiceInterface]
+  (:import [com.google.api.ads.adwords.axis.v201802.mcm CustomerServiceInterface]
            [com.google.api.ads.adwords.axis.factory AdWordsServices]))
 
-(defrecord Customer [company-name currency-code customer-id date-time-zone descriptive-name tracking-url-template test-account?])
+(defrecord Customer [currency-code customer-id date-time-zone descriptive-name tracking-url-template test-account?])
 
 (defn customer-service [adwords-session]
   (let [services (AdWordsServices. )]
     (.get services adwords-session CustomerServiceInterface)))
 
 (defn customer [customer-service]
-  (let [c (.getCustomers customer-service)]
-    (map->Customer {:company-name          (.getCompanyName c)
-                    :currency-code         (.getCurrencyCode c)
-                    :customer-id           (.getCustomerId c)
-                    :date-time-zone        (.getDateTimeZone c)
-                    :descriptive-name      (.getDescriptiveName c)
-                    :tracking-url-template (.getTrackingUrlTemplate c)})))
+  (let [customers (.getCustomers customer-service)]
+    (->> customers
+      (map (fn [c]
+             (map->Customer {:currency-code         (.getCurrencyCode c)
+                             :customer-id           (.getCustomerId c)
+                             :date-time-zone        (.getDateTimeZone c)
+                             :descriptive-name      (.getDescriptiveName c)
+                             :tracking-url-template (.getTrackingUrlTemplate c)}))))))
